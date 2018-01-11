daaf5292c24021726060a32fee173382286fca89
Ruslan <ruslanvs@yahoo.com>
2018-01-10 19:04:57 -0800

    Part 2 done

--- MathDojo.py
+++ MathDojo.py
@@ -3,17 +3,23 @@ class MathDojo( object ):
         self.result = result
 
     def add( self, *args ):
-        self.result += sum( args )
-        # print "add", sum (args)
-        # print "current result", self.result
+        for item in args:
+            if isinstance( item, list ):
+                self.result += sum (item)
+            else:
+                self.result += item
         return self
 
-    def substract( self, *args ):
-        self.result -= sum( args )
-        # print "substract", sum(args)
-        # print "current result", self.result
+    def subtract( self, *args ):
+        for item in args:
+            if isinstance( item, list ):
+                self.result -= sum (item)
+            else:
+                self.result -= item
         return self
 
 md = MathDojo()
 
-print md.add(2).add(2,5).substract(3,2).result
+# print md.add(2).add(2,5).subtract(3,2).result
+
+print md.add([1], 3,4).add([3,5,7,8], [2,4.3,1.25]).subtract(2, [2,3], [1.1,2.3]).result
